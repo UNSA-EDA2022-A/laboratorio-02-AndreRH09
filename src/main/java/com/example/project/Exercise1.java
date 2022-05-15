4291546f278d01c7fddc110d01ee3691109e3128
AndreRH09 <91095214+AndreRH09@users.noreply.github.com>
2022-05-15 11:36:23 -0500

    Update Exercise1.java
    comentarios

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -16,22 +16,21 @@ public class Exercise1 {
 
 	public boolean esCuadradoPerfecto(int numero) {
             
-            if(numero <=0)
+            if(numero <=0)		//El ejercicio no toma al 0 como un cuadrado perfecto y no existen cuadrados negativos
                 return false;
             else{
-                System.out.println(numCuadrado((int) Math.sqrt(numero)));
-               if(numCuadrado((int) Math.sqrt(numero)) == numero)
+               if(numCuadrado((int) Math.sqrt(numero)) == numero)	//Llama al metodo y comprueba si es que el cuadrado encontrado es igual al numero
                    return true;
             }
-            return false;
+            return false;		
 	}
         
-        public int numCuadrado(int n) {
+        public int numCuadrado(int n) {		//Para utilizar la formula recursiva se necesita la raiz entera que significa el n-simo cuadrado 
             
-            if(n == 1)
+            if(n == 1)		//En el caso A(n) = 2 n - 1, para n = 1; A(1) = 1 , o llamdo caso base
                 return 1;
             else{
-                return  2 * n - 1 +numCuadrado(n - 1);
+                return  2 * n - 1 +numCuadrado(n - 1);		//Aqui la formula recursiva es A(n) = 2n - 1 + A(n-1)
             }
         }
 }
